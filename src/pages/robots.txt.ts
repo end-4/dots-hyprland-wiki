ffe3d8d15f40881b3cb1043a1e3e6f17f9cb8042
Celestial.y <clsty@celestialy.top>
2025-10-07 14:08:46 +0800

    Fix robots.txt.ts to support base

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -7,7 +7,9 @@ Allow: /
 Sitemap: ${sitemapURL.href}
 `;
 
-export const GET: APIRoute = ({ site }) => {
-  const sitemapURL = new URL('sitemap-index.xml', site);
+export const GET: APIRoute = ({ site, base }) => {
+  // Remove the first slash from base
+  const normalizedBase = base.startsWith('/') ? base.slice(1, -0) : base;
+  const sitemapURL = new URL(`${normalizedBase}/sitemap-index.xml`, site);
   return new Response(getRobotsTxt(sitemapURL));
 };
